85725c136086e47d965ba9d14554f5ded2d7ebe4
Akash-Yadav-454647 <121929071+Akash-Yadav-454647@users.noreply.github.com>
2025-02-20 23:00:04 +0530

    Time: 67 ms (13.88%) | Memory: 12.6 MB (84.00%) - LeetSync

---
+++ 2107-find-unique-binary-string/find-unique-binary-string.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void solve(string& res, string& temp, vector<string>& vec, int& n,bool & found){
+        if(temp.size()==n){
+            if(!found && find(vec.begin(), vec.end(), temp)==vec.end()){
+                res = temp;
+                found = true;
+            }
+            return;
+        }
+        for(int ch = '0' ; ch<='1'; ch++){
+            temp.push_back(ch);
+            solve(res,temp,vec,n,found);
+            temp.pop_back();
+        }
+    }
+    string findDifferentBinaryString(vector<string>& nums) {
+        map<int,string> mp;
+        int n = nums.size();
+        string res = "";
+        string temp = "";
+        bool found = false;
+        solve(res,temp,nums,n,found);
+        return res;
+    }
+};
