9b8e9729b1d908854b17cf96c1ebf766eef57102
Akash-Yadav-454647 <121929071+Akash-Yadav-454647@users.noreply.github.com>
2025-03-12 23:13:42 +0530

    Time: 6 ms (0.67%) | Memory: 21.4 MB (43.55%) - LeetSync

--- 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.cpp
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.cpp
@@ -1,13 +1,47 @@
 class Solution {
 public:
     int maximumCount(vector<int>& nums) {
-        int positiveCount = 0, negativeCount = 0;
-
-        for (int num : nums) {
-            positiveCount += num > 0;
-            negativeCount += num < 0;
+        int l = 0;
+        int positive = 0;
+        int r = nums.size()-1;
+        while(l<=r){
+            int mid = (l+r)/2;
+            if(nums[mid]==0){
+                positive = mid+1;
+                l = mid+1;
+            }
+            else if(nums[mid]>0){
+                positive = mid;
+                r = mid - 1;
+            }
+            else{
+                l = mid+1;
+            }
         }
-
-        return max(positiveCount, negativeCount);
+        int l1 = 0;
+        int r1 = nums.size()-1;
+        int negative = 0;
+        while(l1<=r1){
+            int mid = (l1+r1)/2;
+            if(nums[mid]==0){
+                negative = mid-1;
+                r1 = mid-1;
+            }
+            else if(nums[mid]>0){
+                r1 = mid - 1;
+            }
+            else{
+                negative = mid;
+                l1 = mid+1;
+            }
+        }
+        cout<<positive<<" "<<negative<<endl;
+        int n = nums.size();
+        int p = n-positive;
+        int n1 = negative+1;
+        if(p>n1){
+            return p;
+        }
+        return n1;
     }
 };
