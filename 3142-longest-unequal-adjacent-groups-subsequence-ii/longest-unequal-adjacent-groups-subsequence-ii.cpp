ade43c71313cf5560c1835a045f8fcb9a3792416
Akash-Yadav-454647 <121929071+Akash-Yadav-454647@users.noreply.github.com>
2025-05-16 21:13:36 +0530

    Time: 119 ms (23.44%) | Memory: 38.5 MB (28.13%) - LeetSync

---
+++ 3142-longest-unequal-adjacent-groups-subsequence-ii/longest-unequal-adjacent-groups-subsequence-ii.cpp
@@ -0,0 +1,70 @@
+class Solution {
+public:
+
+    int dp[1001][1002];
+    vector<string> ans;
+
+    bool fun(const string &x, const string &y) {
+        if (x.size() != y.size()) return false;
+        int cnt = 0;
+        for (int i = 0; i < (int)x.size(); i++) {
+            if (x[i] != y[i] && ++cnt > 1)
+                return false;
+        }
+        return cnt == 1;
+    }
+
+    int solve(int ind, int prev,
+              const vector<string>& w,
+              const vector<int>& g) {
+        int pi = prev + 1, n = w.size();
+        if (ind == n) 
+            return 0;
+        if (dp[ind][pi] != -1) 
+            return dp[ind][pi];
+
+
+        int best = solve(ind + 1, prev, w, g);
+
+
+        if (prev == -1 ||
+            (g[prev] != g[ind] && fun(w[prev], w[ind]))) {
+            best = max(best,
+                       1 + solve(ind + 1, ind, w, g));
+        }
+
+        return dp[ind][pi] = best;
+    }
+
+    vector<string> getWordsInLongestSubsequence(vector<string>& w,
+                                                vector<int>& g) {
+        int n = w.size();
+
+        memset(dp, -1, sizeof(dp));
+
+        solve(0, -1, w, g);
+
+        ans.clear();
+        int ind = 0, prev = -1;
+        while (ind < n) {
+            int pi = prev + 1;
+            int skip = dp[ind + 1][pi];
+            int take = -1;
+
+            if (prev == -1 ||
+                (g[prev] != g[ind] && fun(w[prev], w[ind]))) {
+                take = 1 + dp[ind + 1][ind + 1];
+            }
+
+            if (take > skip) {
+                // choose to take it
+                ans.push_back(w[ind]);
+                prev = ind;
+            }
+ 
+            ind++;
+        }
+
+        return ans;
+    }
+};
