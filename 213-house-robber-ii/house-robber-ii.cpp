6207e8573d92d15288e0733cbdc1edd84953a996
Akash-Yadav-454647 <121929071+Akash-Yadav-454647@users.noreply.github.com>
2025-05-17 12:33:23 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (55.42%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -4,11 +4,11 @@ public:
         int n = nums.size();
         if (n == 0) return 0;
         if (n == 1) return nums[0];
-        return max(robLinear(nums, 0, n - 2), robLinear(nums, 1, n - 1));
+        return max(solve(nums, 0, n - 2), solve(nums, 1, n - 1));
     }
 
 private:
-    int robLinear(vector<int>& nums, int start, int end) {
+    int solve(vector<int>& nums, int start, int end) {
         int len = end - start + 1;
         vector<int> dp(len + 1, 0);
         dp[0] = 0;
