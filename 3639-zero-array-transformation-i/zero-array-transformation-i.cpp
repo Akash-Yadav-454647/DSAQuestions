1189809fd672336270a6453d9e5ed955c33a0895
Akash-Yadav-454647 <121929071+Akash-Yadav-454647@users.noreply.github.com>
2025-05-20 19:19:27 +0530

    Time: 3 ms (87.30%) | Memory: 292.8 MB (86.91%) - LeetSync

---
+++ 3639-zero-array-transformation-i/zero-array-transformation-i.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isZeroArray(vector<int>& n, vector<vector<int>>& q) {
+        vector<int> d(n.size(),0);
+        for(int i = 0 ; i<q.size();i++){
+            int  l = q[i][0];
+            int r = q[i][1];
+            d[l] -= 1;
+            if(r+1<n.size()){
+                d[r+1] += 1;
+            }
+        }
+        for(int i = 1;i<d.size(); i++){
+            d[i] += d[i-1];
+        }
+        for(int i = 0; i<n.size();i++){
+            if(n[i]>0){
+                n[i] += d[i];
+            }
+            if(n[i]> 0){
+                return false;
+            }
+
+        }
+        return true;
+         
+    }
+};
